b0a3eff2fcc7970c83dc44415fd2f3588e2e588c
Momo Ramadori <momoramadori8@gmail.com>
2020-04-23 13:16:31 +0200

    done

---
+++ JSnack5/main.js
@@ -0,0 +1,10 @@
+//L'utente dice un numero N e stampo il cubo di tot numeri fino ad n
+
+var numeriUtente = parseInt(prompt("Calcolo dei cubi fino al numero:"))
+if (isNaN(numeriUtente) == true) {
+    console.log("non hai inserito un numero intero positivo!");
+} else {
+    for (var i = 1; i <= numeriUtente; i++) {
+        console.log(i*i*i);
+    }
+}
---
+++ JSnack6/main.js
@@ -0,0 +1,4 @@
+//Stampa le potenze di 2 fino a 1000
+for (var i = 0; Math.pow(2,i) < 1000; i++) {
+    console.log(Math.pow(2,i));
+}
